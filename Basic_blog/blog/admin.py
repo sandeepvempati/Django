0862c42d225b22b63a567eb8361b6cd9f4334570
sandeepvempati <vempatisandeep189@gmail.com>
2017-07-09 21:39:42 -0500

    Django_blog

---
+++ Basic_blog/blog/admin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib import admin
+from .models import Post
+from django.db import models
+
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','publish','status')
+    list_filter = ('status','created','publish','author')
+    search_fields = ('title','body','status')
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status','publish']
+
+admin.site.register(Post,PostAdmin)
+
+
